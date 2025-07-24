d4191bd3ac564ad2f601a90ba28dd243a45e9d04
GBIS <78549430+GBIS-Lab@users.noreply.github.com>
2025-07-24 10:50:07 +0200

    Update SmartphoneBatchController.java

--- src/main/java/com/smartphonebatch/controller/SmartphoneBatchController.java
+++ src/main/java/com/smartphonebatch/controller/SmartphoneBatchController.java
@@ -10,19 +10,4 @@ public class SmartphoneBatchController {
     @Autowired
     private BatchConfig batchConfig;
 
-    /*
-    @PostMapping("/api/upload")
-    public String uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
-        // Sauvegarder temporairement le fichier
-        String filePath = "path/to/temp/storage/" + file.getOriginalFilename();
-        file.transferTo(new File(filePath));
-
-        // Passer le chemin au lecteur pour démarrer le batch
-        //!!!!!!!!!!!!!!!!!!!!!!!!batchConfig.setFilePathForReader(filePath);
-
-        // Lancer le batch si nécessaire (ou laisser un autre mécanisme pour démarrer le batch)
-        // Exemple : jobLauncher.run(job, parameters);
-
-        return "Fichier reçu et traitement démarré!";
-    }*/
 }
